a3cc390842a16ee36e045cf00de8f59b94b1fb67
KawshikBss <kawshikbss@gmail.com>
2023-05-14 16:25:16 +0600

    created users model

---
+++ server/models/users.js
@@ -0,0 +1,66 @@
+var mongoose = require("mongoose");
+
+var usersSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+    },
+    age: {
+        type: Number,
+    },
+    weight: {
+        type: Number,
+    },
+    height: {
+        type: Number,
+    },
+    goal: {
+        type: String,
+        required: true,
+    },
+    activity_level: {
+        type: String,
+        required: true,
+    },
+    trainer: {
+        type: Boolean,
+        default: false,
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+    member_status: {
+        type: String,
+        required: true,
+    },
+    trainer_rating: {
+        type: Number,
+        default: 0,
+    },
+    membership: {
+        type: Date,
+    },
+    credit: {
+        type: Number,
+        default: 0,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+module.exports = mongoose.model("User", usersSchema);
