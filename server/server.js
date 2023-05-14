eb45da05df5917e402758d94c78c7586711f4a46
KawshikBss <kawshikbss@gmail.com>
2023-05-14 18:59:33 +0600

    added db code

--- server/server.js
+++ server/server.js
@@ -2,6 +2,15 @@ var express = require("express");
 var app = express();
 const port = 3000;
 
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/devmuscles", {
+    useNewUrlParser: true,
+});
+
+var mongooseConnection = mongoose.connection;
+mongooseConnection.on("error", (err) => console.error(err));
+mongooseConnection.once("open", () => console.log("Connected to database"));
+
 var usersRouter = require("./routes/users");
 
 app.get("/", (req, res) => {
