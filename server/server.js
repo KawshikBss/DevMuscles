30909d70527c217b2cd52c04c6afc162e8dcda9d
KawshikBss <kawshikbss@gmail.com>
2023-05-14 15:45:22 +0600

    registered usersRouter

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ var express = require("express");
 var app = express();
 const port = 3000;
 
+var usersRouter = require("./routes/users");
+
 app.get("/", (req, res) => {
     return res.json({ message: "Wellcome to Devmuscles" });
 });
+
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}/`);
