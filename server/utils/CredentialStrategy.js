c475ab509911ecb4d593d31cb2d86894f997c13f
KawshikBss <kawshikbss@gmail.com>
2023-05-16 14:25:52 +0600

    completed credential strat

--- server/utils/CredentialStrategy.js
+++ server/utils/CredentialStrategy.js
@@ -1,1 +1,25 @@
-var LocalStrategy = require('passport-local')
+var LocalStrategy = require("passport-local");
+var UserModel = require("../models/users");
+var bcrypt = require("bcrypt");
+
+var CredentialStrategy = new LocalStrategy(
+    { username: "email", password: "password" },
+    async (username, password, next) => {
+        try {
+            var user = await UserModel.find({ email: username });
+            if (!user) {
+                return next(null, flase, {
+                    message: "You have entered an invalid email address!",
+                });
+            }
+            if (!bcrypt.compare(password, user.password)) {
+                return next(null, false, {
+                    message: "The password you have entered is incorrect!",
+                });
+            }
+            return next(null, user);
+        } catch (err) {
+            return next(err);
+        }
+    }
+);
